5e179b4da45ebde43759d35247716169bcac168d
Egor Kirilin <z@xd.cm>
2018-10-17 17:37:41 +0300

    AuctionContext (UOW) class added.

---
+++ Infrastructure/Data/AuctionContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class AuctionContext : DbContext //UOF
+    {
+        public AuctionContext(DbContextOptions<AuctionContext> options) : base(options)
+        {
+            DbInitializer.Seed(this);
+        }
+
+        public DbSet<Bid> Bids { get; set; }
+        public DbSet<Lot> Lots { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+
+
+    }
+}
