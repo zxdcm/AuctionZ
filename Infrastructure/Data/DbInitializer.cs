ddc9d833b39e02088817dda84a88badcf46bed40
Egor Kirilin <z@xd.cm>
2018-10-17 17:37:14 +0300

    DbInitializer with preconfigured entities added.

---
+++ Infrastructure/Data/DbInitializer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using ApplicationCore.Entities;
+using Microsoft.AspNetCore.Builder;
+
+namespace Infrastructure.Data
+{
+    public static class DbInitializer
+    {
+        public static void Seed(AuctionContext context) // replace with DI later
+        {
+            
+            context.AddRange(GetPreconfiguredCategories());
+            context.AddRange(GetPreconfiguredUsers());
+            context.SaveChanges();
+   
+        }
+
+        private static IEnumerable<User> GetPreconfiguredUsers()
+        {
+            return new List<User>()
+            {
+                new User { Name  = "Justin Noon" },
+                new User { Name = "Willie Par" },
+                new User { Name  = "Martin Noon" },
+                new User { Name  = "Noon Justin" },
+                new User { Name  = "WHil Par" },
+            };
+        }
+
+        private static IEnumerable<Category> GetPreconfiguredCategories()
+        {
+            return new List<Category>()
+            {
+                new Category { Name = "Devices"},
+                new Category { Name = "T-Shirts" },
+                new Category { Name = "Coins" },
+                new Category { Name = "Cars" },
+            };
+        }
+    }
+}
