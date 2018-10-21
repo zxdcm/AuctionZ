af8d9f30eb15be9d34388ae10cb813c292d8eaf7
Egor Kirilin <z@xd.cm>
2018-10-22 01:05:07 +0300

    DbContext added. Repos, logic services and AutoMapper service have been registered.

--- AuctionZ/Startup.cs
+++ AuctionZ/Startup.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using ApplicationCore.Interfaces;
+using AutoMapper;
+using Infrastructure.Data;
+using Infrastructure.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+
 
 namespace AuctionZ
 {
@@ -20,6 +28,7 @@ namespace AuctionZ
         }
 
         public IConfiguration Configuration { get; }
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -33,6 +42,24 @@ namespace AuctionZ
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<AuctionContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+
+            services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+            services.AddScoped<ILotRepository, LotRepository>();
+            services.AddScoped<IBidRepository, BidRepository>();
+
+            services.AddScoped<ILotsService, LotsService>();
+            services.AddScoped<IBidsService, BidsService>();
+
+            services.AddAutoMapper();
+
+            // Add memory cache services
+
+            services.AddMemoryCache();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,15 +75,18 @@ namespace AuctionZ
                 app.UseHsts();
             }
 
+        
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
+
 
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Auction}/{action=Index}/{id?}");
             });
         }
     }
