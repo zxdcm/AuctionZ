60cfa60137aa8a3d02079efddcb68b564db3fa70
Egor Kirilin <z@xd.cm>
2018-10-22 01:00:32 +0300

    ResultListViewModel for pagination added.

---
+++ AuctionZ/Models/Utils/ResultListViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace AuctionZ.Models.Utils
+{
+    public class ResultListViewModel<T> where T: class 
+    {
+        public IEnumerable<T> Items { get; }
+
+        public int PagesNumber { get; }
+
+        public int CurrentPage { get; }
+
+        public ResultListViewModel()
+        {
+
+        }
+
+        public ResultListViewModel(IEnumerable<T> items, int pagesNumbers, int currentPage)
+        {
+            this.Items = items;
+            this.PagesNumber = pagesNumbers;
+            this.CurrentPage = currentPage;
+        }
+
+   
+    }
+}
