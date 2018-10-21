968c1a8a31796df88d53f23b38fd59119d2d64b8
Egor Kirilin <z@xd.cm>
2018-10-22 00:51:40 +0300

    Category model updated. Collection in User entity added.

--- ApplicationCore/Entities/Category.cs
+++ ApplicationCore/Entities/Category.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
+
 namespace ApplicationCore.Entities
 {
     public class Category
     {
         public int CategoryId { get; set; }
-        public string Name { get; set; }   
+        public string Name { get; set; }
+        public ICollection<Lot> Lots { get; set; }
+
     }
 }
