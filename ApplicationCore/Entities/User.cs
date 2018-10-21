3bbb38af1fb257165c79ab4a53d2c51d9aaa4d17
Egor Kirilin <z@xd.cm>
2018-10-22 00:51:02 +0300

    Bid and Lot collections added

--- ApplicationCore/Entities/User.cs
+++ ApplicationCore/Entities/User.cs
@@ -1,9 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+
 namespace ApplicationCore.Entities
 {
     public class User
     {
-        public int UserId { get; set; } 
+        public int UserId { get; set; }
         public int IdentityId { get; set; }
         public decimal Money { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Bid> Bids { get; set; }
+        public ICollection<Lot> Lots { get; set; }
+
+
     }
 }
