fcca6c5b4c1ff102c18d3dd8bfa1600e3f9b630c
Egor Kirilin <z@xd.cm>
2018-10-22 00:53:55 +0300

    OnModelCreating method added. DeleteBehavior for entities tables provided to escape update anomalies

--- Infrastructure/Data/AuctionContext.cs
+++ Infrastructure/Data/AuctionContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using ApplicationCore.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Infrastructure.Data
 {
@@ -19,6 +20,42 @@ namespace Infrastructure.Data
         public DbSet<Role> Roles { get; set; }
         public DbSet<User> Users { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Bid>(ConfigureBids);
+            modelBuilder.Entity<Lot>(ConfigureLot);
+            modelBuilder.Entity<User>(ConfigureUser);
+            modelBuilder.Entity<Category>(ConfigureCategory);
+        }
+
+        private void ConfigureBids(EntityTypeBuilder<Bid> builder)
+        {
+        }
+
+        private void ConfigureLot(EntityTypeBuilder<Lot> builder)
+        {
+            builder
+                .HasOne(x => x.Category)
+                .WithMany(x => x.Lots)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+
+        private void ConfigureUser(EntityTypeBuilder<User> builder)
+        {
+            builder
+                .HasMany(x => x.Bids)
+                .WithOne(x => x.User)
+                .OnDelete(DeleteBehavior.Restrict);
+
+        }
+
+        private void ConfigureCategory(EntityTypeBuilder<Category> builder)
+        {
+            builder
+                .HasMany(x => x.Lots)
+                .WithOne(x => x.Category)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
 
     }
 }
