87e768ca9b517b344edc54658f0a73398544335e
Egor Kirilin <z@xd.cm>
2018-10-24 00:09:52 +0300

    MappingProfile for lot and viewmodel created.

---
+++ AuctionZ/Models/MappingProfiles/LotMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using ApplicationCore.Entities;
+using AutoMapper;
+
+namespace AuctionZ.Models.MappingProfiles
+{
+    public class LotMappingProfile : Profile
+    {
+        public LotMappingProfile()
+        {
+            CreateMap<Lot, LotViewModel>()
+                .ForMember(x => x.Categories, opt => opt.Ignore());
+            CreateMap<LotViewModel, Lot>();
+        }
+    }
+}
