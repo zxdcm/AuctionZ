b66eb29f3bbf33c0594b29be4a9929c55492a549
Egor Kirilin <z@xd.cm>
2018-10-26 22:38:15 +0300

    BidMappingProfile added.

---
+++ AuctionZ/Models/MappingProfiles/BidMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using ApplicationCore.Entities;
+using AutoMapper;
+
+namespace AuctionZ.Models.MappingProfiles
+{
+    public class BidMappingProfile : Profile
+    {
+        public BidMappingProfile()
+        {
+            CreateMap<Bid, BidViewModel>();
+            CreateMap<BidViewModel, Bid>();
+        }
+    }
+}
