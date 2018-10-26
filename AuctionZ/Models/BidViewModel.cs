d3ca80107f7d73853693cdcb4280c11b6226f469
Egor Kirilin <z@xd.cm>
2018-10-26 22:36:59 +0300

    BidViewModel added

--- AuctionZ/Models/BidViewModel.cs
+++ AuctionZ/Models/BidViewModel.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using ApplicationCore.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace AuctionZ.Models
 {
     public class BidViewModel
     {
+        [ScaffoldColumn(false)]
+        public int BidId { get; set; }
+        public DateTime DateOfBid { get; set; }
+        public string Price { get; set; }
+        public int LotId { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
     }
 }
