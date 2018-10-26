769bc68b1ff1a48444240216279be143cf11e8a8
Egor Kirilin <z@xd.cm>
2018-10-26 22:20:29 +0300

    IdentityId and Name prop removed

--- ApplicationCore/Entities/User.cs
+++ ApplicationCore/Entities/User.cs
@@ -6,9 +6,7 @@ namespace ApplicationCore.Entities
     public class User
     {
         public int UserId { get; set; }
-        public int IdentityId { get; set; }
         public decimal Money { get; set; }
-        public string Name { get; set; }
 
         public ICollection<Bid> Bids { get; set; }
         public ICollection<Lot> Lots { get; set; }
