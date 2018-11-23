ff9e1b8215df4ca3cce6bdbcccc63c2c1bc08b15
Egor Kirilin <z@xd.cm>
2018-11-24 01:02:35 +0300

    user inhertis user-identity now

--- ApplicationCore/Entities/User.cs
+++ ApplicationCore/Entities/User.cs
@@ -1,16 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace ApplicationCore.Entities
 {
-    public class User
+    public class User : IdentityUser
     {
-        public int UserId { get; set; }
         public decimal Money { get; set; }
-
+        public string Name { get; set; }
         public ICollection<Bid> Bids { get; set; }
         public ICollection<Lot> Lots { get; set; }
-
-
     }
 }
