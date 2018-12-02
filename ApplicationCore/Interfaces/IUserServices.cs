99ab89c9ca5a1077fd69438fbf0743d7177e1fde
Egor Kirilin <z@xd.cm>
2018-12-02 03:09:01 +0300

    TryRegister, TryLogin, Logout interface declaration added

---
+++ ApplicationCore/Interfaces/IUserServices.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using ApplicationCore.DTO;
+using Microsoft.AspNetCore.Identity;
+
+namespace ApplicationCore.Interfaces
+{
+    public interface IUserServices : IManagementService<UserDto>
+    {
+        void MakeBid(int lotId, int bidderId, decimal bidValue);
+        Task<IdentityResult> TryRegister(UserDto user, string password);
+        Task<SignInResult> TryLogin(string email, string password);
+        Task Logout();
+    }
+}
