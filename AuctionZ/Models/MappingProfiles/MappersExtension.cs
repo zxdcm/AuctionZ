d9c41d8137e6d7af6d5cc044f434857293342ed6
Egor Kirilin <z@xd.cm>
2018-12-02 03:11:31 +0300

    Extensions for Dtos and VM added. RegisterViewModelMappingProfile added.

---
+++ AuctionZ/Models/MappingProfiles/MappersExtension.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using ApplicationCore.DTO;
+using AuctionZ.Models.Account;
+using AutoMapper;
+
+
+namespace AuctionZ.Models.MappingProfiles
+{
+    public static class MappersExtension
+    {
+        public static LotDto ToDto(this LotViewModel vm) => Mapper.Map<LotDto>(vm);
+        public static LotViewModel ToVm(this LotDto dto) => Mapper.Map<LotViewModel>(dto);
+    
+        public static IEnumerable<LotDto> ToDto(this IEnumerable<LotViewModel> vm) 
+            => Mapper.Map<IEnumerable<LotDto>>(vm);
+        public static IEnumerable<LotViewModel> ToVm(this IEnumerable<LotDto> dto) 
+            => Mapper.Map<IEnumerable<LotViewModel>>(dto);
+
+
+        public static BidDto ToDto(this BidViewModel vm) => Mapper.Map<BidDto>(vm);
+        public static BidViewModel ToVm(this BidDto dto) => Mapper.Map<BidViewModel>(dto);
+
+        public static IEnumerable<BidDto> ToDto(this IEnumerable<BidViewModel> vm)
+            => Mapper.Map<IEnumerable<BidDto>>(vm);
+        public static IEnumerable<BidViewModel> ToVm(this IEnumerable<BidDto> dto)
+            => Mapper.Map<IEnumerable<BidViewModel>>(dto);
+
+        public static UserDto ToDto(this RegisterViewModel vm)
+            => Mapper.Map<UserDto>(vm);
+
+    }
+}
---
+++ AuctionZ/Models/MappingProfiles/RegisterViewModelMappingProfile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationCore.DTO;
+using AuctionZ.Models.Account;
+using AutoMapper;
+
+namespace AuctionZ.Models.MappingProfiles
+{
+    public class RegisterViewModelMappingProfile : Profile
+    {
+        public RegisterViewModelMappingProfile()
+        {
+            CreateMap<RegisterViewModel, UserDto>()
+                .ForMember(x => x.UserId, opt => opt.Ignore())
+                .ForMember(x => x.Money, opt => opt.Ignore())
+                .ForMember(x => x.Bids, opt => opt.Ignore())
+                .ForMember(x => x.Lots, opt => opt.Ignore());
+            CreateMap<UserDto, RegisterViewModel>()
+                .ForSourceMember(x => x.UserId, opt => opt.Ignore())
+                .ForSourceMember(x => x.Money, opt => opt.Ignore())
+                .ForSourceMember(x => x.Lots, opt => opt.Ignore())
+                .ForSourceMember(x => x.Bids, opt => opt.Ignore());
+        }
+    }
+}
