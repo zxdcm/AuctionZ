a258268871eba3655f6f5f36df19ef0968eace95
Egor Kirilin <z@xd.cm>
2018-12-05 23:24:40 +0300

    BaseController to receive auth user claims added

---
+++ AuctionZ/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace AuctionZ.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected int UserId => 
+            int.Parse(User?.FindFirst(ClaimTypes.NameIdentifier).Value);
+
+        protected string UserName => User.Identity.Name;
+    }
+}
