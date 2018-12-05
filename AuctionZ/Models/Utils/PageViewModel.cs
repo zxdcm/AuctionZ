a03954c7437854b406353c35d3d8034a7344be06
Egor Kirilin <z@xd.cm>
2018-12-05 23:21:21 +0300

    Pagination tag helper added.

---
+++ AuctionZ/Models/Utils/PageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AuctionZ.Models.Utils
+{
+    public class PageViewModel
+    {
+        public int PageNumber { get; }
+        public int TotalPages { get; }
+
+        public PageViewModel(int count, int pageNumber, int pageSize)
+        {
+            PageNumber = pageNumber;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+        }
+
+        public bool HasPreviousPage => (PageNumber > 1);
+
+        public bool HasNextPage => (PageNumber < TotalPages);
+    }
+}
---
+++ AuctionZ/TagHelpers/PageLinkTagHelper.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuctionZ.Models.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AuctionZ.TagHelpers
+{
+    public class PageLinkTagHelper : TagHelper
+    {
+
+        private readonly IUrlHelperFactory urlHelperFactory;
+
+        public PageLinkTagHelper(IUrlHelperFactory helperFactory)
+        {
+            urlHelperFactory = helperFactory;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+        public PageViewModel PageModel { get; set; }
+        public string PageAction { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+            output.TagName = "div";
+
+            TagBuilder tag = new TagBuilder("ul");
+            tag.AddCssClass("pagination");
+
+            TagBuilder currentItem = CreateTag(PageModel.PageNumber, urlHelper);
+
+            if (PageModel.HasPreviousPage)
+            {
+                TagBuilder prevItem = CreateTag(PageModel.PageNumber - 1, urlHelper);
+                tag.InnerHtml.AppendHtml(prevItem);
+            }
+
+            tag.InnerHtml.AppendHtml(currentItem);
+
+            if (PageModel.HasNextPage)
+            {
+                TagBuilder nextItem = CreateTag(PageModel.PageNumber + 1, urlHelper);
+                tag.InnerHtml.AppendHtml(nextItem);
+            }
+            output.Content.AppendHtml(tag);
+        }
+
+        TagBuilder CreateTag(int pageNumber, IUrlHelper urlHelper)
+        {
+            TagBuilder item = new TagBuilder("li");
+            TagBuilder link = new TagBuilder("a");
+            if (pageNumber == PageModel.PageNumber)
+                item.AddCssClass("active");
+            else
+                link.Attributes["href"] = urlHelper.Action(PageAction, new { page = pageNumber });
+            link.InnerHtml.Append(pageNumber.ToString());
+            item.InnerHtml.AppendHtml(link);
+            return item;
+        }
+    }
+}
