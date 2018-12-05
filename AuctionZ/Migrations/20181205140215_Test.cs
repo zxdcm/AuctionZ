970c3d6dc12cf8dc44bf120e209bc00bd1683dfe
Egor Kirilin <z@xd.cm>
2018-12-05 23:25:40 +0300

    Migrations uploaded.

---
+++ AuctionZ/Migrations/20181205140215_Test.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AuctionZ.Migrations
+{
+    public partial class Test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ AuctionZ/Migrations/20181205142134_DropColumn.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AuctionZ.Migrations
+{
+    public partial class DropColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
