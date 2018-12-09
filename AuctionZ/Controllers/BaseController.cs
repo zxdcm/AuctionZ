1fd0c83de088560e29f29f9794dbe167c5e050a2
Egor Kirilin <z@xd.cm>
2018-12-09 19:37:59 +0300

    GetUserIdByUserName method added.

--- AuctionZ/Controllers/BaseController.cs
+++ AuctionZ/Controllers/BaseController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Security.Claims;
+using ApplicationCore.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 
 namespace AuctionZ.Controllers
@@ -14,5 +17,15 @@ namespace AuctionZ.Controllers
             int.Parse(User?.FindFirst(ClaimTypes.NameIdentifier).Value);
 
         protected string UserName => User.Identity.Name;
+        protected bool IsAdmin => User.IsInRole("admin");
+
+        protected int? GetUserIdByUserName(string userName)
+        {
+            if (userName == null)
+                return null;
+            var manager = HttpContext.RequestServices.GetRequiredService<UserManager<User>>();
+            var id =  manager.FindByNameAsync(userName)?.Id;
+            return id ?? 0;
+        }
     }
 }
