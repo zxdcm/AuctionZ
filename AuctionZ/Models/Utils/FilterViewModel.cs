bec51f343674a0a59eeba69caa1ec423926c329f
Egor Kirilin <z@xd.cm>
2018-12-09 19:24:28 +0300

    ViewModel to filter Lots added.

---
+++ AuctionZ/Models/Utils/FilterViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationCore.DTO;
+using ApplicationCore.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AuctionZ.Models.Utils
+{
+    public class FilterViewModel
+    {
+        public FilterViewModel(IEnumerable<CategoryDto> categories, string title,
+            int? selectedCategory, bool? active=true, string userName=null)
+        {
+            var categoriesList = categories.ToList(); //Fix somehow later
+            categoriesList.Insert(0, new CategoryDto() {Name = "All", CategoryId = 0});
+            Categories = new SelectList(categoriesList, "CategoryId", "Name", selectedCategory);
+            SelectedTitle = title;
+            SelectedCategory = selectedCategory;
+            Active = active;
+            UserName = userName;
+        }
+        public SelectList Categories { get; } 
+        public int? SelectedCategory { get; }   
+        public string SelectedTitle { get; }
+        public bool? Active { get; } 
+        public string UserName { get; }
+    }
+}
