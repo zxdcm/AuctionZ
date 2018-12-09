38c727cb9961bb3ae765d0f361d8f3dc45acb7fd
Egor Kirilin <z@xd.cm>
2018-12-09 19:26:37 +0300

    LotsViewModel added to aggregate LotsViewModels & Pagination & Filtering

---
+++ AuctionZ/Models/LotsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AuctionZ.Models.Utils;
+
+namespace AuctionZ.Models
+{
+    public class LotsViewModel
+    {
+        public IEnumerable<LotViewModel> Lots { get; set; }
+        public PageViewModel Pagination { get; set; }
+        public FilterViewModel Filter { get; set; }
+    }
+}
