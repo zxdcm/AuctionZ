0089ebf9897371ce5849fe1f2bf03fc8f6f20e14
Egor Kirilin <z@xd.cm>
2018-12-09 19:04:26 +0300

    CRUD role repository added

---
+++ ApplicationCore/Interfaces/IRoleRepository.cs
@@ -0,0 +1,8 @@
+﻿using ApplicationCore.Entities;
+
+namespace ApplicationCore.Interfaces
+{
+    public interface IRoleRepository : IRepository<Role>
+    {
+    }
+}
---
+++ Infrastructure/Data/RoleRepository.cs
@@ -0,0 +1,13 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+
+namespace Infrastructure.Data
+{
+    public class RoleRepository : EfRepository<Role>, IRoleRepository
+    {
+        public RoleRepository(AuctionContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
