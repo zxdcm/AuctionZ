1c94759e83bb24639018c577d9edab45823be8be
Egor Kirilin <z@xd.cm>
2018-12-09 19:28:25 +0300

    RoleViewModel & RoleMofificationViewModels added

---
+++ AuctionZ/Models/RoleViewModels.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationCore.DTO;
+using ApplicationCore.Entities;
+
+namespace AuctionZ.Models
+{
+    public class RoleViewModel
+    {
+        public RoleDto Role { get; set; }
+        public IEnumerable<UserDto> Members { get; set; }
+        public IEnumerable<UserDto> NonMembers { get; set; }
+    }
+
+    public class RoleModificationViewModel
+    {
+        [Required]
+        public string RoleName { get; set; }
+        public int RoleId { get; set; }
+        public int[] IdsToAdd { get; set; }
+        public int[] IdsToDelete { get; set; }
+    }
+
+}
