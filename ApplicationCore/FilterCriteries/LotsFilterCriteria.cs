ad692faae0422479fd0500081a69a25c1aa6d9e1
Egor Kirilin <z@xd.cm>
2018-12-09 19:18:33 +0300

    LotFilterCritiria to filtering and pagination added.

---
+++ ApplicationCore/FilterCriteries/LotsFilterCriteria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore
+{
+    public class LotsFilterCriteria
+    {
+        public int? Category { get; set; }
+        public string Title { get; set; }
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 4;
+        public bool? Active { get; set; } = null;
+        public int? UserId { get; set; } = null;
+    }
+}
