cedda49077ae93f02568dfc709978dc1d61d138a
Egor Kirilin <z@xd.cm>
2018-12-09 19:08:44 +0300

    Ctor added

--- ApplicationCore/Entities/Role.cs
+++ ApplicationCore/Entities/Role.cs
@@ -1,14 +1,18 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
 namespace ApplicationCore.Entities
 {
     public class Role : IdentityRole<int>
     {
+
         public int RoleId
         {
             get => Id;
             set => Id = value;
         }
 
+        public Role(string roleName) : base(roleName) { }
+        public Role() {  }
     }
 }
