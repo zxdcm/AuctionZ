19fe144802579225177c4f63be7ca0753483f9b6
Egor Kirilin <z@xd.cm>
2018-12-09 19:19:04 +0300

    Methods were updated. Now they accept critiria that allows to filter data. GetUserPurchaces method added.

--- Infrastructure/Data/LotRepository.cs
+++ Infrastructure/Data/LotRepository.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
+using ApplicationCore;
 using ApplicationCore.Entities;
 using ApplicationCore.Interfaces;
+using AutoMapper.QueryableExtensions;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data
@@ -12,15 +14,42 @@ namespace Infrastructure.Data
     public class LotRepository : EfRepository<Lot>, ILotRepository
     {
 
-        public LotRepository(AuctionContext context) : base(context)
-        {
-
-        }
+        public LotRepository(AuctionContext context) : base(context) { }
 
 
         public IEnumerable<Lot> GetAllLotsForUser(int userId)
         {
             return _context.Lots.Where(x => x.UserId == userId);
+        }
+
+
+        private IQueryable<Lot> FilterByCriteria(LotsFilterCriteria criteria)
+        {
+            var lots = _context.Lots.AsQueryable();
+            if (!string.IsNullOrEmpty(criteria.Title))
+                lots = lots.Where(x => x.Name.Contains(criteria.Title));
+            if (criteria.Category.HasValue)
+                lots = lots.Where(x => x.CategoryId == criteria.Category);
+            if (criteria.Active.HasValue) lots = criteria.Active.Value ? lots.Where(x => x.ExpirationTime > DateTime.Now) : lots.Where(x => x.ExpirationTime < DateTime.Now);
+            if (criteria.UserId.HasValue)
+                lots = lots.Where(x => x.UserId == criteria.UserId);
+            return lots;
+        }
+
+        private IQueryable<Lot> GetLots(LotsFilterCriteria criteria)
+        {
+            var lots = FilterByCriteria(criteria);
+            if (criteria.Page != 0)
+                return lots.Skip((criteria.Page - 1) * criteria.PageSize).Take(criteria.PageSize);
+            return lots;
+        }
+
+
+        public IEnumerable<Lot> GetAllLots(LotsFilterCriteria criteria) => GetLots(criteria);
+
+        public IEnumerable<Lot> GetAllLotsWithUsers(LotsFilterCriteria criteria)
+        {
+            return GetLots(criteria).Include(x => x.User);
         }
 
         public Lot GetByIdWithBids(int id)
@@ -40,6 +69,28 @@ namespace Infrastructure.Data
         public IEnumerable<Lot> FindWithBids(Expression<Func<Lot, bool>> predicate)
         {
             return _context.Lots.Include(b => b.Bids).Where(predicate); // Todo: somehow swap Include and Where 
+        }
+
+        public int GetLotsCount(LotsFilterCriteria criteria)
+        {
+            return FilterByCriteria(criteria).Count();
+        }
+
+
+        public Bid GetLastBidForLot(int lotId)
+        {
+            return _context.Bids //Todo change later
+                .Where(b => b.LotId == lotId)
+                .OrderByDescending(x => x.Price)
+                .FirstOrDefault();
+        }
+
+        public IEnumerable<Lot> GetUserPurchases(int userId)
+        {
+            return _context.Lots
+                .Where(x => x.ExpirationTime <= DateTime.Now && x.UserId != userId)
+                .Where(x => x.Bids.OrderByDescending(y => y.Price).FirstOrDefault() != null &&
+                           x.Bids.OrderByDescending(y => y.Price).FirstOrDefault().UserId == userId).Include(x => x.User);
         }
 
     }
