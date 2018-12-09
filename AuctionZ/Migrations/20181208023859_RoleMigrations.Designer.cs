c9984454c5c10571c7fcb50c765d289736726e38
Egor Kirilin <z@xd.cm>
2018-12-09 20:01:52 +0300

    csproj updated. migrations uploaded.

--- AuctionZ/Migrations/20181208023859_RoleMigrations.Designer.cs
+++ AuctionZ/Migrations/20181208023859_RoleMigrations.Designer.cs
@@ -4,14 +4,16 @@ using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AuctionZ.Migrations
 {
     [DbContext(typeof(AuctionContext))]
-    partial class AuctionContextModelSnapshot : ModelSnapshot
+    [Migration("20181208023859_RoleMigrations")]
+    partial class RoleMigrations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AuctionZ/Migrations/20181208023859_RoleMigrations.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AuctionZ.Migrations
+{
+    public partial class RoleMigrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RoleId",
+                table: "AspNetRoles");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RoleId",
+                table: "AspNetRoles",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
