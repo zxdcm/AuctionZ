bc3807de0445aad36d331f81cf189431e6186242
Egor Kirilin <z@xd.cm>
2018-12-09 19:50:17 +0300

    ExpirationPicker script added.

---
+++ AuctionZ/wwwroot/js/expirationPicker.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+    $('#expirationPicker').datetimepicker({
+        showTodayButton: true,
+        format: 'DD-MM-YYYY HH:mm',
+        showClose: true,
+        showClear: true,
+        toolbarPlacement: 'top',
+        stepping: 15,
+        widgetPositioning: {
+            horizontal: 'right',
+            vertical: 'bottom'
+        }
+    });
+});
